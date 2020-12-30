18bd2a4380673c4a58dba5883302c846ab5404d5
Malo <ndekezi37@gmail.com>
2020-12-30 09:39:55 +0100

    correction

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,34 @@
-let prenom = prompt("donne votre prenom");
-// Affiche uniquement la premiere lettre du prénom
-let justp = prenom.slice(0,1)
-alert(justp)
-// . Affiche la dernière lettre du prénom de la personne
-let dernier = prenom.charAt(prenom.length -1)
-alert(dernier);
+// Exercice 2
 
-let renom = prenom.slice(1)
-alert(renom)
+//1 affiche 1er lettre 
+let prenom = prompt('Votre prénom?').toLocaleLowerCase();
+// let premsLettre = prenom.charAt(0);
+// alert(`Première lettre de ${prenom} = ${premsLettre}`);
 
-let maj = prenom.substring(0,1)+prenom.substring(1,2).toUpperCase()+prenom.substring
-alert(maj)
+//2 affiche dernière lettre
+// let derniereLettre = prenom.charAt(prenom.length -1);
+// alert(`Derniere lettre ${prenom} = ${derniereLettre} `);
 
+//3 Affiche le prénom sans sa 1er lettre 
+// alert(prenom.substring(1))
+
+//4
+// alert(prenom.charAt(0) + prenom.charAt(1).toLocaleUpperCase() + prenom.slice(2));
+
+// let upper = prenom.charAt(1)
+// alert(prenom.replace(upper, upper.toUpperCase()))
+
+//5
+// let lettre = prompt(`Quelle lettre voulez-vous mettre en majuscule dans '${prenom}'`).toLowerCase();
+// alert(prenom.replace(lettre, lettre.toUpperCase()));
+
+//6
+// let firstUpper = prenom.charAt(0);
+// alert(prenom.replace(firstUpper, firstUpper.toUpperCase()));
+// alert(prenom.charAt(0).toUpperCase() + prenom.substring(1))
+
+
+//7
+let nbAl = Math.round(Math.random()*prenom.length);
+
+alert(prenom.replace(prenom.charAt(nbAl), ""));
